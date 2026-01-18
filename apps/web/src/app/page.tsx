925318a58d7577578079d1878db6fa76c916547b
Taras Nurko <tarasnurko84@gmail.com>
2026-01-18 12:38:24 +0200

    feat(web): initialized web app configs

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-zinc-50 dark:bg-zinc-950 flex justify-center">
+      <main className="container py-8">
+        <Button>dsfds</Button>
+      </main>
+    </div>
+  );
+}
